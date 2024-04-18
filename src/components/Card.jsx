39137b161f8b5508ed3263077fea4551ad49b48f
J <jpham26@gmail.com>
2024-04-18 10:30:45 -0700

    Add isFlipped useState to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import '../styles/Card.css';
 
 export default function Card({ key, driver, onClick }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+    onClick();
+  }
+
   return (
     <div className='card' onClick={onClick} key={key}>
       <img src={driver.image} alt={driver.name} />
